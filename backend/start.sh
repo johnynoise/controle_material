6cce80a98a2a75911d39ecf8661dc8d9a3cc1a67
Johny Noise <79932410+johnynoise@users.noreply.github.com>
2025-11-06 16:41:36 -0400

    fix: update MySQL host in start script and refactor Docker configurations for consistency

--- backend/start.sh
+++ backend/start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Waiting for MySQL to be ready..."
-until mysqladmin ping -h"mysql" -P"3306" -u"root" -p"$MYSQL_ROOT_PASSWORD" --silent; do
+until mysqladmin ping -h"db" -P"3306" -u"root" -p"$MYSQL_ROOT_PASSWORD" --silent; do
   echo "MySQL is unavailable - sleeping"
   sleep 1
 done
